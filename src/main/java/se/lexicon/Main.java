033446ced31d7aeb51f73f2ce37dc8856cbf96aa
Simon Elbrink <simon.elbrink@lexicon.se>
2022-06-01 12:00:21 +0200

    Variables

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,8 +1,20 @@
-package se.lexicon;
+package se.lexicon; // Package always at line 1
 
-public class Main {
-    public static void main(String[] args) {
 
+// Single-Line Comment - Text that java Compiler will never look at.
+
+/*
+    Multi-line comment
+ */
+
+/**
+ * Multi-Line Comment, used when documenting you code.
+ */
+
+public class Main { // Start of a class
+    public static void main(String[] args) { //Start of a method
+
+        //Variable
         short shortNumber1 = 11111;
         int number1 = 1_000_000_000;
         long longNumber = 1_000_000_000_000_000_000L;
@@ -10,15 +22,14 @@ public class Main {
 
         double decimalNumber = 100.00;
         float floatDecimal = 300.01F;
-        // int decimals = 100.95;
+        // int decimals = 100.95; // This is a Decimal number can not be saved in type int!
 
-        boolean falsy = false; //0
-        boolean trufy  = true; //1
+        boolean valid = false; //0
+        boolean check = true; //1
 
 
-        char anA = 'a';
+        char anALetter = 'a';
         char character = '\u0044'; //D
-
 
 
         //Create a variable
@@ -31,13 +42,11 @@ public class Main {
         //System.out.println(speed);
 
 
-
-        int myNumber1=1,myNumber2=10,myNumber3;
+        int myNumber1 = 1, myNumber2 = 10, myNumber3;
 
         System.out.println(myNumber1);
         System.out.println(myNumber2);
         //System.out.println(myNumber3);
+    } // End of Method
 
-
-    }
-}
+} //End of the class
