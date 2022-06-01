144f2835cfd2564be14c05dd708cfaed81518c44
Simon Elbrink <simon.elbrink@lexicon.se>
2022-06-01 12:02:14 +0200

    Part for explaining a bit about String Object.

---
+++ src/main/java/se/lexicon/StringExamples.java
@@ -0,0 +1,24 @@
+package se.lexicon;
+
+public class StringExamples {
+
+    public static void main(String[] args) {
+
+/*        String aWord = "Hi";
+        String sentence = "Hello Java Group 43";
+        String aName = "Simon";
+        aName = aName.concat(" Elbrink");
+        System.out.println(aName);
+*/
+
+
+        String firstName = "Simon";
+        String lastName = "Elbrink";
+        int birthYear = 1997;
+
+        String personInfo = firstName + " " + lastName + " Age: " + (2022 - birthYear);
+
+        System.out.println(personInfo);
+
+    }
+}
