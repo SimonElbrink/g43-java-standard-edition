253577d8534f70052c1554211beff20253dec473
Simon Elbrink <simon.elbrink@lexicon.se>
2022-06-01 12:00:50 +0200

    Part for Operators

---
+++ src/main/java/se/lexicon/OperatorExamples.java
@@ -0,0 +1,82 @@
+package se.lexicon;
+
+public class OperatorExamples { // Name of Public Class and File needs to be the same.
+
+
+
+    //To make use of a live template for creating a main method.
+    //psvm + enter
+    //main + enter
+    public static void main(String[] args) {
+
+/*
+        int number1 = 10;
+        int number2 = 5;
+
+//        System.out.println(number1); //10
+//       System.out.println(number2); //5
+
+//        number1 = number2;
+//
+//        char a  = 'A';
+//        number1 = a;
+//        System.out.println(number1);
+
+
+
+        //int number3 = number1 + number2;
+
+
+//        int sumOfN12  = number1 + number2;
+//
+//        int subtraction = number1 - 50;
+//
+//        System.out.println(subtraction);
+
+
+//        number1 = number1 + 5;
+
+//        System.out.println(number1 + 1); // Print out number1 + 1 but does not change number1
+//        System.out.println(number1);
+
+
+//        System.out.println(number1++); // Print out number1 then increment by 1, prints 10
+        System.out.println(++number1); // increment by 1 and the n print out number1, prints 11
+
+
+        int modulus = 10 % 3; //  3 , 6 , 9  3.1
+        System.out.println(modulus);
+*/
+
+
+        int num1 = 15;
+        int num2 = 14;
+
+//        boolean result = num1 == num2;  // 15 The same as 14? = False
+        boolean result = num1 != num2;  //  15 The same as 14? = False  (FLIP) -> True
+//        boolean result = num1 > num2;  // 15 greater than 14? = True
+//        boolean result = num1 < num2;  // 15 lesser than 14? = False
+        System.out.println(result);
+
+/*
+
+        int number1 = 15;
+        int number2 = 15;
+
+        boolean result = number1 >= number2; // is 15 greater or Equal to 15? = True
+        System.out.println(result);
+
+*/
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
