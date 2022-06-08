b85234064c9cb0bc950af4ba69c245053605b11c
Simon Elbrink <simon.elbrink@lexicon.se>
2022-06-08 10:47:54 +0200

    Calculator example

---
+++ calculator/src/main/java/se/lexicon/Calculator.java
@@ -0,0 +1,34 @@
+package se.lexicon;
+
+public class Calculator {
+
+
+    /**
+     *  TODO - Add, Sub, Multi, Div
+     *
+     */
+
+    //Syntax for Method: [Access Modifier] static [return Type] [Method Name] ([Params]) {}
+    public static double addition(double number1, double number2){
+        double result = 0;
+
+        result = number1 + number2;
+
+        return result;
+    }
+
+    public static double subtraction(double number1, double number2){
+        return number1 - number2;
+    }
+
+    public static double multiplication(double number1, double number2){
+        return number1 * number2;
+    }
+
+    public static double division(double number1, double number2){
+        return number1 / number2;
+    }
+
+
+
+}
---
+++ calculator/src/main/java/se/lexicon/Main.java
@@ -0,0 +1,15 @@
+package se.lexicon;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int num1 = 69;
+        int num2 = 9000;
+
+        double resultFromAddition = Calculator.addition(num1, num2);
+
+        double resultFromDivision = Calculator.division(0,60);
+
+        System.out.println(resultFromDivision);
+    }
+}
