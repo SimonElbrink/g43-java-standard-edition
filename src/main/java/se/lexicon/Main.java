d2511e86992406a022ddcffc6aab806cbe0661e2
Simon Elbrink <simon.elbrink@lexicon.se>
2022-06-09 08:35:41 +0200

    Methods Lecture - Added some comments.

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,9 +1,18 @@
 package se.lexicon;
 
 
+import se.lexicon.methods.MethodExamples;
+
 public class Main { //Start of Class
 
     public static void main(String[] args) {
+
+        MethodExamples.guessProcess(30, 40); // Public Access modifier - Can be accessed from any class.
+
+//        MethodExamples.addition(10, 50); // Private protected - Only accessible in MethodExamples
+
+//        MethodExamples.greetings("Java group!"); //Package Protected - Accessible in same package (se.lexicon.methods)
+
 
     }
 
--- src/main/java/se/lexicon/methods/MethodExamples.java
+++ src/main/java/se/lexicon/methods/MethodExamples.java
@@ -2,9 +2,12 @@ package se.lexicon.methods;
 
 import java.util.Scanner;
 
+/**
+ * Method Syntax: [access modifier] static [return type] [method name] ([params]) {}
+ */
 public class MethodExamples {
 
-
+    //Public Access
     public static void guessProcess(int myGuess, int numberToGuess){
         if (myGuess > numberToGuess){
             System.out.println("number to High guess a Lower number");
@@ -15,16 +18,17 @@ public class MethodExamples {
         }
     }
 
-
+    //Private Access
     private static double addition(double num1, double num2){
         return num1 + num2;
     }
 
-
+    //Package Access
     static void greetings(String groupName){
         System.out.println("Greetings " + groupName);
     }
 
+    //Public Access
     public static String getStringFromUser(String prompt){
         Scanner sc = new Scanner(System.in);
 
@@ -32,40 +36,36 @@ public class MethodExamples {
         return sc.nextLine();
     }
 
+    public static void main(String[] args) {
 
+//        greetings("JavaGroup43");
+
+//        double number1 = 10;
+//        double number2 = 15;
+//
+//        double result = 0;
+//
+//        result = addition(number1, number2);
+//
+//        System.out.println(result);
+
+
+        MethodExamples.guessProcess(30, 40); // Public Access modifier - Can be accessed from any class.
+
+        MethodExamples.addition(10, 50); // Private protected - Only accessible in this class (MethodExamples)
+
+        MethodExamples.greetings("Java group!"); //Package Protected - Accessible in same package (se.lexicon.methods)
+
+
+        String string = MethodExamples.getStringFromUser("Enter a String: ");
+
+        System.out.println("You Entered: " + string);
+
+
+        String antherString = MethodExamples.getStringFromUser("Enter AntherOne: ");
+
+        System.out.println(antherString);
+
+
+    }
 }
-
-
-//    public static void main(String[] args) {
-//
-////        greetings("JavaGroup43");
-//
-////        double number1 = 10;
-////        double number2 = 15;
-////
-////        double result = 0;
-////
-////        result = addition(number1, number2);
-////
-////        System.out.println(result);
-//
-//
-//        MethodExamples.guessProcess(30, 40);
-//
-//
-////        MethodExamples.addition(10, 50); // Private prtected
-//
-////        MethodExamples.greetings("Java group!"); //Package Protected.
-//
-//
-//        String string = MethodExamples.getStringFromUser("Enter a String: ");
-//
-//        System.out.println("You Entered: " + string);
-//
-//
-//        String antherString = MethodExamples.getStringFromUser("Enter AntherOne: ");
-//
-//        System.out.println(antherString);
-//
-//
-//    }
