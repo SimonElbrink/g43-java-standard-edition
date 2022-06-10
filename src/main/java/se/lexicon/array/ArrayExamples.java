eda0b75bbde99c8735b0cb5ca523b322e533b9a3
Simon Elbrink <simon.elbrink@lexicon.se>
2022-06-10 11:22:03 +0200

    Array Lecture - UPDATE  recap

--- src/main/java/se/lexicon/array/ArrayExamples.java
+++ src/main/java/se/lexicon/array/ArrayExamples.java
@@ -1,5 +1,7 @@
 package se.lexicon.array;
 
+import java.util.Arrays;
+
 public class ArrayExamples {
 
     public static void main(String[] args) {
@@ -10,7 +12,9 @@ public class ArrayExamples {
 
 //        iteratingArray();
 
-        multidimensionalArray();
+//        multidimensionalArray();
+
+        TwoDStringArray();
     }
 
     public static void initializing() {
@@ -58,38 +62,63 @@ public class ArrayExamples {
     public static void iteratingArray() {
 
         //Fill array with values of 1 to 10.
-        int[] numbers = new int[10];
+        int[] numbers = new int[10]; // [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = i + 1;
         }
+        // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
 
 //        for (int i = 0; i < numbers.length; i++){ // Access Value AND INDEX
 //            System.out.println("Numbers Array of index " + i + " With Value "+ numbers[i]);
 //        }
 
+        //print out the array.
         for (int numberInArray : numbers) {
-            System.out.println(numberInArray); // Only access Value NO Index
+
+//            if (numberInArray % 2 == 0){
+//                System.out.print(numberInArray + " "); // Only access Value NO Index
+//            }
+//            if (numberInArray % 2 != 0){
+//                System.out.print(numberInArray + " "); // Only access Value NO Index
+//            }
+
+//            if (numberInArray == 6){
+//                System.out.print(numberInArray + " "); // Only access Value NO Index
+//            }
+
+            System.out.print(numberInArray + " "); // Only access Value NO Index
         }
+
+        //Print out the Array
+//        System.out.println(Arrays.toString(numbers));
 
     }
 
     public static void multidimensionalArray() {
 
-        int[][] numbers = new int[3][3];
+//        int[][] numbers = new int[3][3];
+//
+//        numbers[0][0] = 1;
+//        numbers[0][1] = 10;
+//        numbers[0][2] = 100;
+//
+//        numbers[1][0] = 2;
+//        numbers[1][1] = 20;
+//        numbers[1][2] = 200;
+//        //numbers[1][3] = 2000; - RunTime Error
+//
+//        numbers[2][0] = 3;
+//        numbers[2][1] = 30;
+//        numbers[2][2] = 300;
 
-        numbers[0][0] = 1;
-        numbers[0][1] = 10;
-        numbers[0][2] = 100;
-
-        numbers[1][0] = 2;
-        numbers[1][1] = 20;
-        numbers[1][2] = 200;
-
-        numbers[2][0] = 3;
-        numbers[2][1] = 30;
-        numbers[2][2] = 300;
+        int[][] numbers = {
+                {1,10,100},
+                {2,20,200,2000},
+                {3,30,300,3000,30_000}
+        };
 
 
         for (int i = 0; i < numbers.length; i++) {
@@ -100,5 +129,34 @@ public class ArrayExamples {
             System.out.println(" ");
         }
 
+    }
+    public static void TwoDStringArray() {
+        String[] [] strArr = new String[2][2];
+        // [null,null]
+        // [null,null]
+
+        strArr[0][0] = "France";
+        strArr[0][1] ="Paris";
+        // [France, Paris]
+        // [null, null]
+
+        strArr[1][0] = "Norway";
+        strArr[1][1] ="Oslo";
+        // [France, Paris]
+        // [Norway, Oslo]
+
+        //print out 2d Array
+        for (int i=0; i < strArr.length; i++){
+            for(int j=0; j < strArr[i].length; j++){
+                System.out.print(strArr[i][j]);
+
+            }
+            System.out.println();
+        }
+
+        System.out.println(strArr[0][0] + " " + strArr[0][1]);
+        System.out.println(strArr[1][0] + " " + strArr[1][1]);
+
+        System.out.println(Arrays.deepToString(strArr));
     }
 }
--- src/main/java/se/lexicon/array/ArrayUtilExamples.java
+++ src/main/java/se/lexicon/array/ArrayUtilExamples.java
@@ -6,6 +6,12 @@ public class ArrayUtilExamples {
 
     public static void main(String[] args) {
 
+//        ex1();
+
+        combineArrays();
+    }
+
+    private static void ex1() {
         String[] names = {
                 "Christopher Ojaide",
                 "Husayn Ganiev",
@@ -46,6 +52,59 @@ public class ArrayUtilExamples {
         Arrays.sort(names);
 
         System.out.println(Arrays.toString(names));
+    }
+
+
+    public static void combineArrays(){
+        String[] group43students = {
+                "Christopher Ojaide",
+                "Husayn Ganiev",
+                "Jonas Eira",
+                "Julia Lindman",
+                "Julius Büchner",
+                "Nader Alhamwi",
+                "Patrik Axelsson",
+                "Roudabeh Adnani",
+                "Mohsen Forouzmand",
+                "Ta Thi Ly",
+                "Anusha Yenugu",
+                "Roberto Camacho Hakkarainen",
+                "Thanapat Sombatkamrai",
+                "Zeq Alidemaj",
+                "Joe Yaghiv",
+                "Jack Ninway",
+                "Jennie Henriksson Smith",
+                "Rosita Axelsson",
+                "Nazifa Husseini"
+        };
+
+        String[] staffAtLexicon= {
+                "Simon Elbrink",
+                "Mehrdad Javan",
+                "Åsa Jonsson",
+                "Marcus Gudmundsen",
+                "Kent Gudmundsen",
+        };
+
+//        group43students[group43students.length] = staffAtLexicon[0];
+
+        String[] lexiconSociety = Arrays.copyOf(
+                group43students,
+                group43students.length + staffAtLexicon.length); //19 + 5 new length
+
+
+        System.out.println(Arrays.toString(lexiconSociety));
+
+        //Where to start adding form
+        int index = group43students.length;
+
+        //Add staff to society
+        for (int i = 0; i < staffAtLexicon.length;i++){
+            lexiconSociety[index] = staffAtLexicon[i];
+            index++;
+            System.out.println(Arrays.toString(lexiconSociety));
+        }
+
     }
 
 
