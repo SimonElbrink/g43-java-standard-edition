ffa27a1e65a90696d482e4a6711c72f2c9a148ab
Simon Elbrink <simon.elbrink@lexicon.se>
2022-06-27 09:50:18 +0200

    Static versus Instance Basics Examples

---
+++ static-instance/src/main/java/se/lexicon/Main.java
@@ -0,0 +1,58 @@
+package se.lexicon;
+
+public class Main {
+
+    //Fields
+    public static int number1 = 10;
+
+
+    public static void main(String[] args)
+    {
+
+//        method1();
+        Main.method1(); // Access in a static way.
+
+
+//        instanceMethod();// compile error.
+
+        Main main = new Main();// Instantiating an Object.
+        main.instanceMethod();
+
+        System.out.println(number1);// print out 10
+        System.out.println(Main.number1);// print out 10
+
+
+        System.out.println(VolvoCar.brand); // prints static field = Volvo.
+
+        VolvoCar v70 = new VolvoCar(); // Instance of a Car saved in a variable names v70.
+        v70.model = "v70";
+        v70.regNumber ="ABC123";
+
+        System.out.println(v70.getInformation());
+
+        VolvoCar xc90 = new VolvoCar();
+        xc90.model = "xc90";
+        xc90.regNumber = "CBA123";
+
+        System.out.println(xc90.getInformation());
+
+        VolvoCar.brand = "VOLVO CARS";  // Changes the value in all instances of a VolvoCar.
+        System.out.println("Changes the brand name.");
+
+        System.out.println(v70.getInformation());
+        System.out.println(xc90.getInformation());
+
+    }
+
+    public static void method1(){
+        System.out.println("Static method");
+
+//        instanceMethod(); // Can not call instance method
+    }
+
+    public void instanceMethod(){
+        System.out.println("Instance Method");
+
+        method1(); // Inside a instance method you can call a static method
+    }
+}
---
+++ static-instance/src/main/java/se/lexicon/VolvoCar.java
@@ -0,0 +1,15 @@
+package se.lexicon;
+
+public class VolvoCar {
+    //Static field can be accessed using the class Name ex VolvoCar.Brand
+    //Instance Field ca be accessed for a specific object ex this.model
+
+    public static String brand = "Volvo"; // Static field
+    public String model; // Instance Field
+    public String regNumber; // Instance Field
+
+    public String getInformation(){
+        return "Brand: " + VolvoCar.brand + " Model: " + this.model + " RegistrationNumber: " + this.regNumber;
+    }
+
+}
